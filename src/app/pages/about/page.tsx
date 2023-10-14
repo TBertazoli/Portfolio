15cbcefee3c834807a9924534f0745c81298d5b1
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2023-10-14 00:56:26 -0500

    will fix css later for about me

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -20,33 +20,29 @@ export default function About() {
             <TbArrowBackUpDouble />
           </Link>
         </div>
-        <div>
-          <div className={styles.description_left}>
-            <p>
-              Greetings! I'm Tatiana Bertazoli, a Junior Software Developer with
-              a unique journey. Originally from Brazil, I initially pursued a
-              degree in Chemistry. However, in 2010, my family and I relocated
-              to Canada, where we spent 8 wonderful years. In 2018, we made
-              another life-changing move, this time to Texas, and that's when I
-              decided to embark on a new career path in web development. I took
-              the plunge in 2022 by enrolling in a bootcamp at Rice University.
-              Since then, I've been diligently working on various small personal
-              projects to enhance my skills and prepare for a career in this
-              exciting field. My projects have been crafted using a variety of
-              technologies, including HTML, CSS, JavaScript, Node.js, APIs, SQL,
-              NoSQL, Express.js, and React. I'm eager to continue learning and
-              grow as a web developer.
-            </p>
-          </div>
-          <div className={styles.description_right}>
-            <Image
-              src={profilePic}
-              width={400}
-              alt="profile"
-              className={styles.profile_pic}
-              height={600}
-            ></Image>
-          </div>
+
+        <div className={styles.description}>
+          <Image
+            src={profilePic}
+            width={400}
+            alt="profile"
+            className={styles.profile_pic}
+          ></Image>
+          <p>
+            Greetings! I'm Tatiana Bertazoli, a Junior Software Developer with a
+            unique journey. Originally from Brazil, I initially pursued a degree
+            in Chemistry. However, in 2010, my family and I relocated to Canada,
+            where we spent 8 wonderful years. In 2018, we made another
+            life-changing move, this time to Texas, and that's when I decided to
+            embark on a new career path in web development. I took the plunge in
+            2022 by enrolling in a bootcamp at Rice University. Since then, I've
+            been diligently working on various small personal projects to
+            enhance my skills and prepare for a career in this exciting field.
+            My projects have been crafted using a variety of technologies,
+            including HTML, CSS, JavaScript, Node.js, APIs, SQL, NoSQL,
+            Express.js, and React. I'm eager to continue learning and grow as a
+            web developer.
+          </p>
         </div>
 
         <Skills></Skills>
