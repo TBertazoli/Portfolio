9e7eb84c193255db8b673d031dbad1fce48e0f8e
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2023-10-15 20:00:00 -0500

    fixed escaping on about me

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -29,19 +29,19 @@ export default function About() {
             className={styles.profile_pic}
           ></Image>
           <p>
-            Greetings! I'm Tatiana Bertazoli, a Junior Software Developer with a
-            unique journey. Originally from Brazil, I initially pursued a degree
-            in Chemistry. However, in 2010, my family and I relocated to Canada,
-            where we spent 8 wonderful years. In 2018, we made another
-            life-changing move, this time to Texas, and that's when I decided to
-            embark on a new career path in web development. I took the plunge in
-            2022 by enrolling in a bootcamp at Rice University. Since then, I've
-            been diligently working on various small personal projects to
-            enhance my skills and prepare for a career in this exciting field.
-            My projects have been crafted using a variety of technologies,
-            including HTML, CSS, JavaScript, Node.js, APIs, SQL, NoSQL,
-            Express.js, and React. I'm eager to continue learning and grow as a
-            web developer.
+            Greetings! I&apos;m Tatiana Bertazoli, a Junior Software Developer
+            with a unique journey. Originally from Brazil, I initially pursued a
+            degree in Chemistry. However, in 2010, my family and I relocated to
+            Canada, where we spent 8 wonderful years. In 2018, we made another
+            life-changing move, this time to Texas, and that&apos;s when I
+            decided to embark on a new career path in web development. I took
+            the plunge in 2022 by enrolling in a bootcamp at Rice University.
+            Since then, I&apos;ve been diligently working on various small
+            personal projects to enhance my skills and prepare for a career in
+            this exciting field. My projects have been crafted using a variety
+            of technologies, including HTML, CSS, JavaScript, Node.js, APIs,
+            SQL, NoSQL, Express.js, and React. I&apos;m eager to continue
+            learning and grow as a web developer.
           </p>
         </div>
 
