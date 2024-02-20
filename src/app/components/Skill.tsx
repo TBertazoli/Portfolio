595509cc75cdcc42a45fc65a3063035d309f3e1a
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-02-19 21:59:53 -0600

    adding material ui on about pages

--- src/app/components/Skill.tsx
+++ src/app/components/Skill.tsx
@@ -2,31 +2,77 @@
 
 import React from "react";
 import styles from "../page.module.css";
-import ListGroup from "react-bootstrap/ListGroup";
+import Grid from "@mui/material/Grid";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import ListSubheader from "@mui/material/ListSubheader";
 
 function Skills() {
+  const [open, setOpen] = React.useState(true);
+  const [openFront, setOpenFront] = React.useState(true);
+
+  const handleClickFront = () => {
+    setOpenFront(!open);
+  };
+
+  const handleClickBack = () => {
+    setOpen(!open);
+  };
+
   return (
     <section>
       <div className={styles.title}>
         <h2>Skills</h2>
       </div>
       <div className={styles.description}>
-        <ListGroup className={styles.skills}>
-          <ListGroup.Item>HTML</ListGroup.Item>
-          <ListGroup.Item>CSS</ListGroup.Item>
-          <ListGroup.Item>JavaScript</ListGroup.Item>
-          <ListGroup.Item>JQuery</ListGroup.Item>
-          <ListGroup.Item>Responsive design</ListGroup.Item>
-          <ListGroup.Item>React</ListGroup.Item>
-          <ListGroup.Item>Bootstrap</ListGroup.Item>
-          <ListGroup.Item>API</ListGroup.Item>
-          <ListGroup.Item>Node.js</ListGroup.Item>
-          <ListGroup.Item>Express.js</ListGroup.Item>
-          <ListGroup.Item>MySQL</ListGroup.Item>
-          <ListGroup.Item>Sequelize</ListGroup.Item>
-          <ListGroup.Item>MoongoDB</ListGroup.Item>
-          <ListGroup.Item>React</ListGroup.Item>
-        </ListGroup>
+        <Grid item xs={12} md={6}>
+          <List
+            sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+          >
+            <ListItemButton onClick={handleClickFront}>
+              <ListItemText primary="Front End Tech Stack" />
+              {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={openFront} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding sx={{ pl: 4 }}>
+                <ListItemText primary="HTML" />
+                <ListItemText primary="CSS" />
+                <ListItemText primary="JavaScript" />
+                <ListItemText primary="TypeScript" />
+                <ListItemText primary="JQuery" />
+                <ListItemText primary="React" />
+                <ListItemText primary="Next.js" />
+                <ListItemText primary="Bootstrap" />
+                <ListItemText primary="Material-UI" />
+              </List>
+            </Collapse>
+
+            <ListItemButton onClick={handleClickBack}>
+              <ListItemText primary="Back End Tech Stack" />
+              {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding sx={{ pl: 4 }}>
+                <ListItemText primary="Node.js" />
+                <ListItemText primary="Express.js" />
+                <ListItemText primary="MySQL" />
+                <ListItemText primary="MongoDB" />
+                <ListItemText primary="Sequelize" />
+                <ListItemText primary="GraphQL" />
+                <ListItemText primary="Next.js" />
+                <ListItemText primary="Bootstrap" />
+                <ListItemText primary="Material-UI" />
+              </List>
+            </Collapse>
+          </List>
+        </Grid>
       </div>
     </section>
   );
