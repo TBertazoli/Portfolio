4e17b0d09e88b9b6a32e633347ffa20795dcd101
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-05-09 13:54:38 -0500

    updated list of projects

--- src/app/components/Projects.ts
+++ src/app/components/Projects.ts
@@ -1,56 +1,57 @@
 export const projects = [
   {
-    name: "Prework Study Guide",
-    description: "This project was an introduction of HTML and CSS",
+    name: "Placeholder for project 4",
+    description: "Placeholder for project 4",
     website: "https://tbertazoli.github.io/prework-study-guide/",
     repository: "https://github.com/TBertazoli/prework-study-guide",
     image: "/assets/projects/0.png",
   },
   {
-    name: "Run Buddy",
+    name: "FoodMetrix",
     description:
-      "This project was to develop a complete website using only HTML and CSS",
-    website: "https://tbertazoli.github.io/run-buddy/",
-    repository: "https://github.com/TBertazoli/run-buddy",
-    image: "/assets/projects/1.png",
+      "FoodMetrics is a user-friendly online platform meticulously crafted to simplify the tracking of your nutritional intake. This app was developed using Python, Django, Bootstrap, Neon and PostgreSQL.",
+    website: "https://foodmacros-1443b0816711.herokuapp.com/",
+    repository: "https://github.com/TBertazoli/foodmacros",
+    image: "/assets/projects/food-metrics.png",
   },
   {
-    name: "Horiseon",
-    description: "",
-    website: "https://tbertazoli.github.io/Horiseon/",
-    repository: "https://github.com/TBertazoli/Horiseon",
-    image: "/assets/projects/2.png",
-  },
-  {
-    name: "Pizza Hunt",
+    name: "PetVexVault",
     description:
-      "This project wa to develop a website where you can create new pizzas and add comments",
-    website: "https://pizza-bertazoli.herokuapp.com/",
-    repository: "https://github.com/TBertazoli/pizza-hunt",
-    image: "/assets/projects/3.png",
+      "VetVaxVault is a mobile user-friendly website designed to streamline the storage and retrieval of crucial information about your beloved pets. This app was developed using HTML, CSS, JavaScript, Node.js, Express, MongoDB, Mongoose, Boostrap, Google Oauth.",
+    website: "https://vet-records-64d6176bd1f1.herokuapp.com/",
+    repository: "https://github.com/TBertazoli/vet-records",
+    image: "/assets/projects/pet-vex-vault.png",
   },
-  {
-    name: "Project Fitness",
-    description:
-      "On this project I was responsible for writting the front-end JavaScript functionality. Created local storage and functions to store the weight, height and date. Created a function to calculate BMI and added a calendar to track the workout progress page.",
-    website: "https://tbertazoli.github.io/Project-Fitness/",
-    repository: "https://github.com/TBertazoli/Project-Fitness",
-    image: "/assets/projects/4.png",
-  },
-  {
-    name: "Lab Notebook",
-    description:
-      "This project was to develop a virtual lab notebook where users can create projects and experiments",
-    website: "https://lab-notes.herokuapp.com",
-    repository: "https://github.com/emsaw721/lab-rats",
-    image: "/assets/projects/5.png",
-  },
-  {
-    name: "Oinc",
-    description:
-      "This project was to develop a budget app where users can add income and expenses and add budget",
-    website: "https://oinc.herokuapp.com",
-    repository: "https://github.com/emsaw721/stacks-on-stacks",
-    image: "/assets/projects/6.png",
-  },
+  // {
+  //   name: "Pizza Hunt",
+  //   description:
+  //     "This project wa to develop a website where you can create new pizzas and add comments",
+  //   website: "https://pizza-bertazoli.herokuapp.com/",
+  //   repository: "https://github.com/TBertazoli/pizza-hunt",
+  //   image: "/assets/projects/3.png",
+  // },
+  // {
+  //   name: "Project Fitness",
+  //   description:
+  //     "On this project I was responsible for writting the front-end JavaScript functionality. Created local storage and functions to store the weight, height and date. Created a function to calculate BMI and added a calendar to track the workout progress page.",
+  //   website: "https://tbertazoli.github.io/Project-Fitness/",
+  //   repository: "https://github.com/TBertazoli/Project-Fitness",
+  //   image: "/assets/projects/4.png",
+  // },
+  // {
+  //   name: "Lab Notebook",
+  //   description:
+  //     "This project was to develop a virtual lab notebook where users can create projects and experiments",
+  //   website: "https://lab-notes.herokuapp.com",
+  //   repository: "https://github.com/emsaw721/lab-rats",
+  //   image: "/assets/projects/5.png",
+  // },
+  // {
+  //   name: "Oinc",
+  //   description:
+  //     "This project was to develop a budget app where users can add income and expenses and add budget",
+  //   website: "https://oinc.herokuapp.com",
+  //   repository: "https://github.com/emsaw721/stacks-on-stacks",
+  //   image: "/assets/projects/6.png",
+  // },
 ];
--- src/app/pages/portfolio/page.tsx
+++ src/app/pages/portfolio/page.tsx
@@ -16,10 +16,11 @@ import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
 import Avatar from "@mui/material/Avatar";
 import IconButton, { IconButtonProps } from "@mui/material/IconButton";
-import { autocompleteClasses } from "@mui/material";
+import Grid from "@mui/material/Grid";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import AspectRatio from "@mui/joy/AspectRatio";
 import Contact from "@/app/components/Contact";
+import Item from "@mui/material/Grid";
 
 // import FavoriteIcon from "@mui/icons-material/Favorite";
 // import ShareIcon from "@mui/icons-material/Share";
@@ -45,53 +46,79 @@ export default function Portfolio() {
   };
 
   return (
-    <Container maxWidth="md">
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <a href="/">
-          <h2>Portfolio</h2>
-        </a>
-        <Contact></Contact>
-      </div>
-      <div>
-        {isModalOpen && (
-          <ModalComp onClose={onClose} currentProject={currentProject} />
-        )}
-      </div>
-      <div className={styles.description_portfolio}>
-        <Carousel
-          activeIndex={index}
-          onSelect={handleSelect}
-          interval={null}
-          className={styles.carousel}
-        >
-          {projects.map((project, index) => (
-            <Carousel.Item key={project.name}>
-              <Card
-                raised
-                sx={{
-                  maxWidth: 500,
-                  margin: "0 auto",
-                  padding: "0.1em",
-                }}
-              >
-                <Image
-                  src={project.image}
-                  alt={project.name}
-                  width={500}
-                  height={500}
-                />
-
-                <Carousel.Caption
-                  className={styles.caption}
-                  onClick={() => openLink(project, index)}
+    <>
+      {/* <Container maxWidth="md">
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <a href="/">
+            <h2>Portfolio</h2>
+          </a>
+          <Contact></Contact>
+        </div>
+        <div>
+          {isModalOpen && (
+            <ModalComp onClose={onClose} currentProject={currentProject} />
+          )}
+        </div>
+        <div className={styles.description_portfolio}>
+          <Carousel
+            activeIndex={index}
+            onSelect={handleSelect}
+            interval={null}
+            className={styles.carousel}
+          >
+            {projects.map((project, index) => (
+              <Carousel.Item key={project.name}>
+                <Card
+                  raised
+                  sx={{
+                    maxWidth: 500,
+                    margin: "0 auto",
+                    padding: "0.1em",
+                  }}
                 >
-                  <h3 className={styles.project_name}>{project.name}</h3>
-                </Carousel.Caption>
-              </Card>
-            </Carousel.Item>
-          ))}
-        </Carousel>
-      </div>
-    </Container>
+                  <Image
+                    src={project.image}
+                    alt={project.name}
+                    width={500}
+                    height={500}
+                  />
+
+                  <Carousel.Caption
+                    className={styles.caption}
+                    onClick={() => openLink(project, index)}
+                  >
+                    <h3 className={styles.project_name}>{project.name}</h3>
+                  </Carousel.Caption>
+                </Card>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </div>
+      </Container> */}
+      <Container maxWidth="md">
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <a href="/">
+            <h2>Portfolio</h2>
+          </a>
+          <Contact></Contact>
+        </div>
+
+        {projects.map((project, index) => (
+          <Grid container rowSpacing={2} columnSpacing={1} key={index}>
+            <Grid md={6}>
+              <Item>{project.description}</Item>
+            </Grid>
+            <Grid md={6}>
+              <Image
+                src={project.image}
+                alt={project.name}
+                width={250}
+                height={250}
+              />
+            </Grid>
+          </Grid>
+        ))}
+      </Container>
+    </>
   );
 }
